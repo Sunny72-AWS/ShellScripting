528f33d43873ffcb9fdd3689a331c57bf6929e20
Sunny72-AWS <saivignesh120702@gmail.com>
2026-02-23 17:56:36 +0530

    Update DiskSize.sh

--- DiskSize.sh
+++ DiskSize.sh
@@ -2,6 +2,6 @@
 echo "This program get first 10 biggest file in the file system passed via positional argument"
 path="$1"
 echo $path
-du -ah $path | sort -hr | head -n 5 > /pmt/vignesh.txt
+du -ah $path | sort -hr | head -n 5 > /tmp/filesize.txt
 echo "This is the list of big files in the file system $path "
-cat /pmt/vignesh.txt
+cat /tmp/filesize.txt
